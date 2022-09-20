cb8dea935b5b99bb84fac668237e34fe38164045
amon-cofie <ddd.amon123@gmail.com>
2022-09-20 14:40:31 +0000

    Add javascript functions for adding and removing books

--- app.js
+++ app.js
@@ -1,53 +1,60 @@
+// const books = JSON.parse(localStorage.setItem(''))
+
+/*Set the item in the local storage*/
 let collection = [];
 const bookShelf = document.querySelector("#book-shelf");
+form = document.querySelector("#library-controls");
 
 function ReadBook(bTitle, bAuthor) {
   this.title = bTitle;
   this.author = bAuthor;
+  this.Id = Math.floor(Math.random() * 1000000);
 }
 
 // const permanentRemove = function () {};
 
-function addToShelf() {
-  collection.forEach((e) => {
-    bookShelf.appendChild(e);
-  });
-}
-
-const addBook = function () {
-  const titleEntry = document.querySelector("#title").value;
-  const authorEntry = document.querySelector("#author").value;
-  const newAdd = document.createElement("li");
+function addToShelf(e) {
+  const newAdd = document.createElement("div");
   const titleTag = document.createElement("p");
   const authorTag = document.createElement("p");
   const removeBtn = document.createElement("button");
+  const lineSeparator = document.createElement("hr");
+  titleTag.innerText = e.title;
+  authorTag.innerText = e.author;
   removeBtn.innerText = "Remove";
-  const newBook = new ReadBook(titleEntry, authorEntry);
-  titleTag.innerText = newBook.title;
-  authorTag.innerText = newBook.author;
+
   newAdd.appendChild(titleTag);
   newAdd.appendChild(authorTag);
   newAdd.appendChild(removeBtn);
+  newAdd.id = e.Id;
+  bookShelf.appendChild(newAdd);
+  bookShelf.appendChild(lineSeparator);
   collection.push(newAdd);
-  console.log(collection);
-  addToShelf();
+  // console.log(collection);
+  removeBtn.addEventListener("click", () => {
+    removeBook(newAdd);
+    bookShelf.removeChild(lineSeparator);
+  });
+}
+
+const createNewBook = function () {
+  const titleEntry = document.querySelector("#title").value;
+  const authorEntry = document.querySelector("#author").value;
+  const newBook = new ReadBook(titleEntry, authorEntry);
+  // collection.push(newBook);
+  addToShelf(newBook);
+  // form.reset();
 };
 
-// const removeBook = function (unwantedBook) {
-//   collection = collection.filter((e) => {
-//     e.title != unwantedBook.title;
-//     e.author != unwantedBook.author;
-//   });
-// };
-
-//  removeBtn.addEventListener("click", () => {
-//    removeBook(newAdd);
-//    permanentRemove(newAdd);
-//  });
-
-document.querySelector("#library-controls").addEventListener("submit", (e) => {
+form.addEventListener("submit", (e) => {
   e.preventDefault();
-  addBook();
+  createNewBook();
 });
 
-console.log(bookShelf);
+const removeBook = function (book) {
+  console.log(book.id);
+  collection = collection.filter((e) => {
+    e.Id != book.id;
+  });
+  bookShelf.removeChild(book);
+};
