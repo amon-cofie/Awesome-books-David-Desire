1ac96d6258ba65f7f52f2e38182a9897b034528c
amon-cofie <ddd.amon123@gmail.com>
2022-09-21 11:13:43 +0000

    update app.js file

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
-const bookShelf = document.querySelector("#book-shelf");
-const form = document.querySelector("#library-controls");
-let collection = JSON.parse(localStorage.getItem("bookStorageArr")) || [];
+const bookShelf = document.querySelector('#book-shelf');
+const form = document.querySelector('#main-form');
+
+// ensure that the existing storage in memory is loaded into the array
+const collection = JSON.parse(localStorage.getItem('bookStorageArr')) || [];
 
 // defining a dynamic update function for the local storage
 function dynamicStorageUpdate(storeArr) {
-  localStorage.setItem("bookStorageArr", JSON.stringify(storeArr));
+  localStorage.setItem('bookStorageArr', JSON.stringify(storeArr));
 }
 
 // defining the constructor for new book objects to be added
@@ -14,16 +16,32 @@ function ReadBook(bTitle, bAuthor) {
   this.Id = Math.floor(Math.random() * 1000000);
 }
 
-// the function that creates the node and adds it to the bookShelf
+// the function that will remove the book from the screen
+function removeBook(book) {
+  bookShelf.removeChild(book);
+}
+
+// the function that will remove the book from the local storage
+function removeLocal(bookId) {
+  collection.forEach((e) => {
+    if (e.Id === bookId) {
+      const indexArr = collection.indexOf(e);
+      collection.splice(indexArr, 1);
+    }
+  });
+  dynamicStorageUpdate(collection);
+}
+
+// defining the function that creates the node and adds it to the bookShelf
 function addToShelf(e) {
-  const newAdd = document.createElement("div");
-  const titleTag = document.createElement("p");
-  const authorTag = document.createElement("p");
-  const removeBtn = document.createElement("button");
-  const lineSeparator = document.createElement("hr");
+  const newAdd = document.createElement('div');
+  const titleTag = document.createElement('p');
+  const authorTag = document.createElement('p');
+  const removeBtn = document.createElement('button');
+  const lineSeparator = document.createElement('hr');
   titleTag.innerText = e.title;
   authorTag.innerText = e.author;
-  removeBtn.innerText = "Remove";
+  removeBtn.innerText = 'Remove';
   newAdd.appendChild(titleTag);
   newAdd.appendChild(authorTag);
   newAdd.appendChild(removeBtn);
@@ -32,14 +50,12 @@ function addToShelf(e) {
   bookShelf.appendChild(lineSeparator);
 
   // adding the event listener to the remove button of each book
-  removeBtn.addEventListener("click", () => {
+  removeBtn.addEventListener('click', () => {
     removeBook(newAdd);
     bookShelf.removeChild(lineSeparator);
     removeLocal(e.Id);
     // dynamicStorageUpdate(collection);
   });
-  // dynamicStorageUpdate();
-  // console.log(collection);
 }
 
 // function that will get from storage and add to the collection
@@ -49,50 +65,23 @@ function populateCollection() {
   });
 }
 
+// invoking the function to display the books on screen
 populateCollection();
-// checking if the bookStorageArr array is empty in local storage
-// if (!localStorage.getItem("bookStorageArr")) {
-//   dynamicStorageUpdate();
-// } else {
-//   populateCollection();
-// }
 
 // the function that creates the new book object from the constructor
 const createNewBook = function () {
-  const titleEntry = document.querySelector("#title").value;
-  const authorEntry = document.querySelector("#author").value;
+  const titleEntry = document.querySelector('#title').value;
+  const authorEntry = document.querySelector('#author').value;
   const newBook = new ReadBook(titleEntry, authorEntry);
   collection.push(newBook);
   addToShelf(newBook);
   dynamicStorageUpdate(collection);
 };
 
-form.addEventListener("submit", (e) => {
+// adding the submit event listener to the form
+form.addEventListener('submit', (e) => {
   e.preventDefault();
   createNewBook();
+
+  form.reset();
 });
-
-// function filterArr(bookId) {
-//   let filtrate = collection.filter((e) => {
-//     e.Id != bookId;
-//     console.log(e);
-//     console.log(e.Id);
-//     console.log(bookId);
-//   });
-//   console.log(filtrate);
-//   return filtrate;
-// }
-
-function removeBook(book) {
-  bookShelf.removeChild(book);
-}
-
-// localStorage.clear();
-function removeLocal(bookId) {
-  console.log("okay");
-  console.log(bookId);
-  // collection = collection.filter((e) => {
-  //   e.Id != bookId;
-  // });
-  dynamicStorageUpdate(collection);
-}
